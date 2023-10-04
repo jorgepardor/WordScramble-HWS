cc7d8c012a6676c24ec165d06bfbba7b53519564
Jorge Pardo <hi@jorgepardo.dev>
2023-10-04 10:02:15 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Jorge Pardo on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
